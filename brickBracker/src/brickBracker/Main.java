42ad26c2044dad8093d87a0a5f4ecbe58edff916
krishnakant <61001421+Cool-Krish@users.noreply.github.com>
2022-08-11 13:52:06 +0530

    Add files via upload

---
+++ brickBracker/src/brickBracker/Main.java
@@ -0,0 +1,20 @@
+package brickBracker;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame obj = new JFrame();
+		Gameplay gamePlay = new Gameplay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("krishna");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+
+	}
+
+}
